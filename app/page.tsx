9ce040679a63349934cf8c0fae09724c62665680
Pichaipusit Kamsorn <Pichaipusit.ks@gmail.com>
2024-02-28 17:09:51 +0700

    6-QZ Full page

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="grid grid-cols-2">
+      {/* Make this layout https://youtu.be/NJNOHU_ADko?t=198 */}
       <General />
     </main>
   );
