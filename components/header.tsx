31ddd51d0e33ee45608711199e6a99051b684336
Pichaipusit Kamsorn <Pichaipusit.ks@gmail.com>
2024-02-28 12:58:04 +0700

    4-QZ Header

---
+++ components/header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <div>
+      {/* Create this UI https://youtu.be/6aoi2j_izP8?t=514 */}
+      {/* Don't let sidebar move when u scroll */}
+      <p></p>
+    </div>
+  );
+}
