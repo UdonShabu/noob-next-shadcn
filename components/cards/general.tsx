47b495bfa10094f9f2b22c8e9dd55b94afb58694
Pichaipusit Kamsorn <Pichaipusit.ks@gmail.com>
2024-02-28 14:10:06 +0700

    5-QZ Bump chart

---
+++ components/cards/general.tsx
@@ -0,0 +1,9 @@
+export default function General() {
+  return (
+    <div>
+      <div>
+        {/* Use Nivo to create this https://youtu.be/eIRqGui1vQc?t=602 */}
+      </div>
+    </div>
+  );
+}
