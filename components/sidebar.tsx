8e0803751d4f0ccaa0c1afce57674533b4223cb1
Pichaipusit Kamsorn <Pichaipusit.ks@gmail.com>
2024-02-28 10:00:39 +0700

    2-SO UserItem

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,7 +1,11 @@
+import UserItem from "./user-item";
+
 export default function Sidebar() {
   return (
-    <div className="flex flex-col min-w-[300px] min-h-screen border-r">
-      <div>user</div>
+    <div className="flex flex-col min-w-[300px] min-h-screen border-r gap-4">
+      <div>
+        <UserItem />
+      </div>
       <div className="grow ">Menu</div>
       <div>Setting</div>
     </div>
--- components/user-item.tsx
+++ components/user-item.tsx
@@ -1,7 +1,13 @@
 export default function UserItem() {
   return (
-    <div className="">
-      {/* Create this UI https://youtu.be/ydPpcR85UCg?t=655 */}
+    <div className="mx-auto w-[90%] border rounded-md p-4 mt-4 flex  gap-x-3">
+      <div className="min-w-12 min-h-12 rounded-full bg-emerald-500 flex justify-center items-center text-white">
+        <p>OL</p>
+      </div>
+      <div>
+        <p className="font-bold">Olive</p>
+        <p className="text-neutral-500">codewithme@gmail.com</p>
+      </div>
     </div>
   );
 }
