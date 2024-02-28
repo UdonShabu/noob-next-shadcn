7e5a662a234e00cdeabf15c66f676601949add80
Pichaipusit Kamsorn <Pichaipusit.ks@gmail.com>
2024-02-28 18:18:32 +0700

    8-SO Table

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Calendar from "@/components/cards/calendar";
 import General from "@/components/cards/general";
+import { TableDemo } from "@/components/cards/table-demo";
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
 import Image from "next/image";
@@ -14,10 +15,10 @@ export default function Home() {
           <Calendar />
         </div>
       </div>
-      <div className="grid grid-cols-3 gap-8">
+      <div className="grid grid-cols-3 gap-8 overflow-hidden">
         <Card className=" w-full h-[280px]">One</Card>
-        <Card className=" w-full h-[280px]">
-          {/* Make Table scrollable inside itself  */}
+        <Card className=" w-full h-[280px] overflow-scroll">
+          <TableDemo />
         </Card>
         <Card className=" w-full h-[280px]">Three</Card>
       </div>
